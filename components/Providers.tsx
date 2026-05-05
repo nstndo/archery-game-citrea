6e1fd04d8338cea7bce77517e4fe4d60985a8a4d
nstndo <nstndo@gmail.com>
2026-05-05 11:53:27 +0300

    Update Providers.tsx
    Chain fix for correct minting

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -6,7 +6,7 @@ import { walletConnect, injected } from 'wagmi/connectors';
 import { type ReactNode, useState } from 'react';
 import { defineChain } from 'viem';
 
-const citrea = defineChain({
+export const citrea = defineChain({
   id: 4114,
   name: 'Citrea Mainnet',
   nativeCurrency: { name: 'cBTC', symbol: 'cBTC', decimals: 18 },
@@ -18,28 +18,34 @@ const citrea = defineChain({
   },
 });
 
-export function Providers({ children }: { children: ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
-  const [config] = useState(() =>
-    createConfig({
-      chains: [citrea],
-      connectors: [
-        walletConnect({
-          projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '0',
-          metadata: {
-            name: 'Citrea Archery',
-            description: 'Become a Citrea Legend',
-            url: 'https://citrea-archery-game.vercel.app',
-            icons: ['https://citrea-archery-game.vercel.app/favicon.svg'],
-          },
-        }),
-        injected(),
-      ],
-      transports: {
-        [citrea.id]: http('https://rpc.mainnet.citrea.xyz'),
+export const config = createConfig({
+  chains: [citrea],
+  ssr: true,
+  connectors: [
+    walletConnect({
+      projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '0',
+      metadata: {
+        name: 'Citrea Archery',
+        description: 'Become a Citrea Legend',
+        url: 'https://citrea-archery-game.vercel.app',
+        icons: ['https://citrea-archery-game.vercel.app/favicon.svg'],
       },
-    })
-  );
+    }),
+    injected(),
+  ],
+  transports: {
+    [citrea.id]: http('https://rpc.mainnet.citrea.xyz'),
+  },
+});
+
+export function Providers({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  }));
 
   return (
     <WagmiProvider config={config}>
